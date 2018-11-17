5c194860e67348fffa4fd684526caf0800921e9a
Your Name <rsmith5901@gmail.com>
2018-11-17 10:29:24 -0500

    adding iot hub

---
+++ Functions/SoilMoistureProcessor.cs
@@ -0,0 +1,20 @@
+﻿using IoTHubTrigger = Microsoft.Azure.WebJobs.EventHubTriggerAttribute;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public static class SoilMoistureProcessor
+    {
+        [FunctionName("SoilMoistureProcessor")]
+        public static void Run([IoTHubTrigger("messages/events", Connection = "IoTHubConnection")]EventData message, ILogger log)
+        {
+            log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+        }
+    }
+}
